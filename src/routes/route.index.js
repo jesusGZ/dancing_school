bc835c57a87d103cf1f6ec440d091302bf155b14
jesusGZ <j.gr.zurita@gmail.com>
2021-10-18 10:06:43 -0500

    eliminacion de archivos

--- src/routes/route.index.js
+++
@@ -1,9 +0,0 @@
-const glob = require('glob')
-const path = require('path')
-
-module.exports = (app, token) => {
-    glob.sync('./*.js').forEach(file => {
-        if(!file.includes('route.index.js'))
-            require(path.resolve(file))(app, token)
-    });
-}
